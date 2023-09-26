7507a7d628ffcdeb283ed360641b15aadc3b3bdc
Kenny Jrt <jarretkenny@gmail.com>
2023-09-26 19:45:04 +0200

    First component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import MyTitle from './components/MyTitle';
+import PokemonCard from './components/PokemonCard';
+
+function App() {
+  return (
+    <div>
+      <PokemonCard/>
+    </div>
+  );
+}
+
+export default App
